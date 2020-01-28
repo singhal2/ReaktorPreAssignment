7466b304357a8418ad450f679a9ff4ee00a87b70
singhal2 <abhimanyu.singhal@yahoo.com>
2020-01-29 01:05:03 +0200

    Add project files.

---
+++ ReaktorPreAssignment/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using ReaktorPreAssignment.Service;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReaktorPreAssignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            FileService service = new FileService();
+            service.ReadFile();
+            return View();
+        }
+
+        private void ReadFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ReaktorPreAssignment/Models/Package.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReaktorPreAssignment.Models
+{
+    public class PackageItem
+    {
+        public string Name { get; set; }
+        public string Decription { get; set; }
+        public List<string> DependsOn { get; set; }
+        public List<string> RequiredBy{ get; set; }
+    }
+    public class Packages
+    {
+        public List<PackageItem> List { get; set; }
+    }
+}
